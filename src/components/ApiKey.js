c1b2a8a1e5dcd6b304bc3a78b86e1804451dba99
Mai Nguyen <nnmai0606@gmail.com>
2023-11-21 20:09:28 +1030

    feat: instruction on getting api key

--- src/components/ApiKey.js
+++ src/components/ApiKey.js
@@ -1,4 +1,6 @@
 // Define a function named ApiKey
+// Go to https://openweathermap.org/
+// Clik on your account name -> My API keys -> Generate free API key
 const ApiKey = () => {
     return (
         '[APIkey]'
